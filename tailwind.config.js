3f54ce1141f127a145f9d036de320f049920f4d4
rh-pr <ruslana.hural.j@gmail.com>
2023-05-31 00:23:58 +0300

    made login with nextAuth.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        twitterWhite: '#e7e9ea',
+        twitterBlue: '#308cd8',
+        twitterBorder: '#2f3336',
+        twitterLightGray: '#71767b',
+        twitterDarkGray: '#17181c',
+      },
+    },
   },
   plugins: [],
 };
